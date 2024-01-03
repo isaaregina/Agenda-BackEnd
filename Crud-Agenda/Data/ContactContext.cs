805638e49fced197807248d1425689b59062b517
isaaregina <isabellaregina344@gmail.com>
2024-01-02 22:54:01 -0300

    feat: ContactContext Criado

---
+++ Crud-Agenda/Data/ContactContext.cs
@@ -0,0 +1,17 @@
+﻿using Crud_Agenda.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Agenda.Data
+{
+    public class ContactContext : DbContext
+    {
+        public DbSet<ContactModel> contacts { get; set; };
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          => optionsBuilder.UseNpgsql(
+              "Server=localhost;" +
+              "Port=5432;Database=contacts;" +
+              "User Id=postgres;" +
+              "Password=root2023;");
+    }
+}
