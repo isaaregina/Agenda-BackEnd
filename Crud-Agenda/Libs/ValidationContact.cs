1dbd3178139277f123d16ab4d1b60ab90b3668dd
isaaregina <isabellaregina344@gmail.com>
2024-01-03 00:31:37 -0300

    feat: ValidatorContact Criado

---
+++ Crud-Agenda/Libs/ValidationContact.cs
@@ -0,0 +1,16 @@
+﻿using Crud_Agenda.Model;
+
+namespace Crud_Agenda.Libs
+{
+    public class ValidationContact
+    {
+        public static void Validation(ContactModel contact)
+        {
+            if (contact == null) throw new ArgumentException("Preencha todos os campos");
+            if (string.IsNullOrWhiteSpace(contact.name)) throw new ArgumentException("Preencha o campo nome");
+            if (string.IsNullOrWhiteSpace(contact.email)) throw new ArgumentException("Preencha o campo email");
+            if (string.IsNullOrWhiteSpace(contact.phone)) throw new ArgumentException("Preencha o campo telefone");
+
+        }
+    }
+}
