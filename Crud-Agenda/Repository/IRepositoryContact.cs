3c7b76d5d6a781c905d11b0b02f9d827e71c1a65
isaaregina <isabellaregina344@gmail.com>
2024-01-02 22:42:18 -0300

    feat: Criado IRepositoryContact

---
+++ Crud-Agenda/Repository/IRepositoryContact.cs
@@ -0,0 +1,16 @@
+﻿using Crud_Agenda.Model;
+
+namespace Crud_Agenda.Repository
+{
+    public interface IRepositoryContact
+    {
+        void GetAllContact();
+        void GetByEmail(string email);
+        void CreateContact(ContactModel contact);
+        void UpdateContact(ContactModel contact);
+        void DeleteContact(ContactModel contact);
+
+
+
+    }
+}
