fe6eab2c84d83687b6dc83607f36af7cc54216ad
isaaregina <isabellaregina344@gmail.com>
2024-01-03 00:32:03 -0300

    feat: ContactService criado

---
+++ Crud-Agenda/Services/ContactService.cs
@@ -0,0 +1,64 @@
+﻿using Crud_Agenda.Data;
+using Crud_Agenda.Libs;
+using Crud_Agenda.Model;
+using Crud_Agenda.Repository;
+
+namespace Crud_Agenda.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IRepositoryContact repository;
+
+        private readonly ContactContext context;
+
+        public ContactService(IRepositoryContact repository, ContactContext context)
+        {
+            this.repository = repository;
+            this.context = context;
+        }
+
+        public void CreateContact(ContactModel Contact)
+        {
+            ValidationContact.Validation(Contact);
+
+            this.repository.CreateContact(Contact);
+
+        }
+
+        public void DeleteContact(int id, ContactModel Contact)
+        {
+            var contact = this.repository.GetById(id);
+
+            if (contact == null) throw new ArgumentException("Usuário não encontrado");
+
+            this.repository.DeleteContact(Contact);
+        }
+
+        public List<ContactModel> GetAllContact()
+        {
+            return this.repository.GetAllContact();      
+        }
+
+        public async Task<ContactModel> GetByEmail(string Email)
+        {
+            var contact = await this.repository.GetByEmail(Email);
+
+            if (contact == null) throw new ArgumentException("Usuário não encontrado");
+
+            return await this.repository.GetByEmail(Email);
+        }
+
+        public async void UpdateContact(int id, ContactModel Contact)
+        {
+            ValidationContact.Validation(Contact);
+
+            var contact = await this.repository.GetById(id);
+
+            contact.name = Contact.name;
+            contact.email = Contact.email;
+            contact.phone = Contact.phone;
+           
+            this.repository.UpdateContact(contact);
+        }
+    }
+}
---
+++ Crud-Agenda/Services/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using Crud_Agenda.Model;
+
+namespace Crud_Agenda.Services
+{
+    public interface IContactService
+    {
+        List<ContactModel> GetAllContact();
+        Task<ContactModel> GetByEmail(string Email);
+        void CreateContact(ContactModel Contact);
+        void UpdateContact(int id, ContactModel Contact);
+        void DeleteContact(int id, ContactModel Contact);
+    }
+}
