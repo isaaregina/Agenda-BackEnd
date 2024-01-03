8ecb395031f7fa164bad4bea7d2293a6e13d19cd
isaaregina <isabellaregina344@gmail.com>
2024-01-02 22:35:17 -0300

    feat: Criado ContactModel

---
+++ Crud-Agenda/Model/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud_Agenda.Model
+{
+    [Table("contacts")]
+    public class ContactModel
+    {
+        [Key]
+
+        public string name { get; private set; }
+        public string email { get; private set; }
+        public string phone { get; private set; }
+
+    }
+}
