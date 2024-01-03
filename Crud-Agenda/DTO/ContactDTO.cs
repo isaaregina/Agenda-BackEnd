e67d5b96aeb6814303238b94349cc7a6f505b811
isaaregina <isabellaregina344@gmail.com>
2024-01-02 23:27:52 -0300

    feat: ContactDTO adicionado

---
+++ Crud-Agenda/DTO/ContactDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Crud_Agenda.DTO
+{
+    public class ContactDTO
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string email { get; set; }   
+        public string phone { get; set; }
+    }
+}
