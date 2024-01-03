4122f3499dc87b8548a6869d66b78f1fba65f6d4
isaaregina <isabellaregina344@gmail.com>
2024-01-02 23:32:12 -0300

    feat: Mappers incluido

---
+++ Crud-Agenda/Mappers/ContactMappers.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Crud_Agenda.DTO;
+using Crud_Agenda.Model;
+
+namespace Crud_Agenda.Mappers
+{
+    public class ContactMappers : Profile
+    {
+        public ContactMappers() 
+        {
+            CreateMap<ContactModel, ContactDTO>();
+        }
+    }
+}
