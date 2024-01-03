0fd138d2cc4947746454e079c9ccc3bea57d120c
isaaregina <isabellaregina344@gmail.com>
2024-01-02 23:58:49 -0300

    feat: Criado Controller

---
+++ Crud-Agenda/Controller/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_Agenda.Controller
+{
+    public class ContactController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
