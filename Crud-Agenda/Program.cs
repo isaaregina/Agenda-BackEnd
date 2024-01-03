cbd4f6c0cf5a195fc34c2cef12d4fab9059ee6c8
isaaregina <isabellaregina344@gmail.com>
2024-01-02 23:23:58 -0300

    feat: Adicionado injeção de dependência - Program.cs

--- Crud-Agenda/Program.cs
+++ Crud-Agenda/Program.cs
@@ -1,3 +1,5 @@
+using Crud_Agenda.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Injeção de dependência
+builder.Services.AddTransient<IRepositoryContact, RepositoryContact>();
+
 
 var app = builder.Build();
 
